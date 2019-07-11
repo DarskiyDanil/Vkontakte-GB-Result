faa53dde622392e0d7a401c22b790c274e1a4fd1
DarskiyDanil <dandi_dk@mail.ru>
2019-07-11 20:04:14 +0300

    1

---
+++ Vkontakte-GB-Result/Network/NewsService.swift
@@ -0,0 +1,9 @@
+//
+//  NewsService.swift
+//  Vkontakte-GB-Result
+//
+//  Created by Danil Darskiy on 26.06.2019Wednesday.
+//  Copyright © 2019 Danil Darskiy-GB-Result. All rights reserved.
+//
+
+import Foundation
---
+++ Vkontakte-GB-Result/News/NewsCell/NewsCell.swift
@@ -0,0 +1,9 @@
+//
+//  NewsCell.swift
+//  Vkontakte-GB-Result
+//
+//  Created by Danil Darskiy on 26.06.2019Wednesday.
+//  Copyright © 2019 Danil Darskiy-GB-Result. All rights reserved.
+//
+
+import Foundation
