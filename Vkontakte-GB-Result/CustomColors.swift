fb997f320653716896b26bd6ac737ca8a82372df
DarskiyDanil <dandi_dk@mail.ru>
2019-08-12 18:52:48 +0300

    добавил лайки

---
+++ Vkontakte-GB-Result/CustomColors.swift
@@ -0,0 +1,9 @@
+//
+//  CustomColors.swift
+//  Vkontakte-GB-Result
+//
+//  Created by Danil Darskiy on 11.08.2019Sunday.
+//  Copyright © 2019 Danil Darskiy-GB-Result. All rights reserved.
+//
+
+import Foundation
